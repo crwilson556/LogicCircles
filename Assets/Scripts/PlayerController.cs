36f4a8d7e6dcadee644138ba201d519fe1deccbd
crwilson556 <c.r.wilson556@gmail.com>
2023-04-09 11:53:55 -0400

    player rotate and triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool right;
     
     void Start()
     {
-        
+        if(!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
     }
 
    
     void Update()
     {
         transform.Translate(Vector3.right * speed);
-        transform.Rotate(Vector3.forward); //back right, forward left
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        Debug.Log(dn.current);
+
+        //check direction
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if(dn.current == "right")
+        {
+            dir = Vector3.back;
+            rot = 90;
+        }
+        else if (dn.current == "left")
+        {
+            dir = Vector3.forward;
+            rot = 90;
+        }
+
+        transform.Rotate(dir * rot); //back right, forward left
     }
 }
